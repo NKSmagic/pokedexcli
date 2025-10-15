5a20aab397532fe406a2dc2f8cf4c7d63d06e7a2
NKSmagic <schoell.niklas@gmail.com>
2025-10-15 13:33:47 +0200

    cch1l4

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var input string
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			break
+		}
+		input = scanner.Text()
+		result := cleanInput(input)
+		if len(result) > 0 {
+			fmt.Printf("Your command was: %s\n", result[0])
+		}
+	}
+}
+
+func cleanInput(text string) []string {
+	lowered := strings.ToLower(text)
+	result := strings.Fields(lowered)
+	return result
 }
---
+++ repl_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(c.expected) != len(actual) {
+			t.Errorf("The actual slice is not equal to the expected slice")
+			t.Fail()
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("The words do not match")
+				t.Fail()
+			}
+		}
+	}
+}
