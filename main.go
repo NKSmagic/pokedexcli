3ce0523fb00f6e4e9a0831e84e4b02273b5ea28b
NKSmagic <schoell.niklas@gmail.com>
2025-10-20 10:03:09 +0200

    CH1 - done

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	var input string
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		ok := scanner.Scan()
-		if !ok {
-			break
-		}
-		input = scanner.Text()
-		result := cleanInput(input)
-		if len(result) > 0 {
-			fmt.Printf("Your command was: %s\n", result[0])
-		}
-	}
-}
-
-func cleanInput(text string) []string {
-	lowered := strings.ToLower(text)
-	result := strings.Fields(lowered)
-	return result
+	startRepl()
 }
